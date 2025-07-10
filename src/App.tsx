b53c4af07bfb0cd86805118bd2072beabef53f05
Hyunsoo_kim <red_panda119@naver.com>
2025-07-11 04:21:29 +0900

    feat : 메인페이지 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import { type JSX } from "react";
-import { BrowserRouter,  Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Header from "./components/header";
-import CreatePostPage from "./components/page/create-post-page/inex";
+import Header from "@/components/header";
+import MainPage from "@/components/page/main";
+import CreatePostPage from "@/components/page/create-post-page/inex";
 
 import styles from "./App.module.scss";
 
 const App = (): JSX.Element => {
-
   return (
     <div className={styles["app-component"]}>
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" />
+          <Route path="/" element={<MainPage />} />
           <Route path="/create-post" element={<CreatePostPage />} />
         </Routes>
       </BrowserRouter>
