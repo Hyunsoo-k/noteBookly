1e0ad8a738e3fdbc588255546c1475e522dd0923
Hyunsoo_kim <red_panda119@naver.com>
2025-08-16 01:15:08 +0900

    styles : 파일 이동

--- src/components/carousel/carousel-box/index.tsx
+++ src/components/carousel/carousel-box/index.tsx
@@ -1,72 +1,30 @@
 import type { JSX } from "react";
 import { useRef } from "react";
-import { Link } from "react-router-dom";
-import { PiArrowLeftThin } from "react-icons/pi";
-import { PiArrowRightThin } from "react-icons/pi";
 
-import { mockData } from "@/mock-data/carousel";
+import useGetPostListQuery from "@/hooks/query/post-list/use-get-post-list-query";
+import type { ResponsePost } from "@/types/query";
 import CarouselItem from "@/components/carousel/carousel-item";
+import CarouselArrow from "@/components/carousel/carousel-arrow";
 
 import styles from "./index.module.scss";
 
 const CarouselBox = (): JSX.Element => {
+  const { data: queryData } = useGetPostListQuery();
+
   const carouselRef = useRef<HTMLUListElement | null>(null);
-  const isScrollActive = useRef<boolean>(false);
-
-  const handleClickArrow = (direction: "left" | "right"): void => {
-    if (isScrollActive.current) return;
-    isScrollActive.current = true;
-
-    const $carousel = carouselRef.current;
-    if (!$carousel) return;
-
-    const post = $carousel.firstElementChild as HTMLElement;
-    if (!post) return;
-
-    const postWidth = 270;
-    const gap = 25;
-    const scrollAmount = postWidth + gap;
-
-    $carousel.scrollBy({
-      left: direction === "left" ? -scrollAmount : scrollAmount,
-      behavior: "smooth",
-    });
-
-    setTimeout(() => {
-      isScrollActive.current = false;
-    }, 300);
-  };
 
   return (
-    <article ref={carouselRef} className={styles["carousel-component"]}>
-      <ul className={styles["post-box"]}>
-        {mockData.map((postData: any, index: number) => (
-          <li key={index} className={styles["post"]}>
-            <Link to="/">
-              <CarouselItem postData={postData} />
-            </Link>
+    <article ref={carouselRef} className={styles["carousel-box-component"]}>
+      <ul className={styles["carousel-item-box"]}>
+        {queryData?.postList.map((post: ResponsePost, index: number) => (
+          <li key={index} className={styles["carousel-item-wrapper"]}>
+            <CarouselItem post={post} />
           </li>
         ))}
       </ul>
-      <div className={styles["scroll-button-box"]}>
-        <button
-          type="button"
-          onClick={() => {
-            handleClickArrow("left");
-          }}
-          className={styles["scroll-button"]}
-        >
-          <PiArrowLeftThin size={80} color="#666666" />
-        </button>
-        <button
-          type="button"
-          onClick={() => {
-            handleClickArrow("right");
-          }}
-          className={styles["scroll-button"]}
-        >
-          <PiArrowRightThin size={80} color="#666666" />
-        </button>
+      <div className={styles["carousel-arrow-box"]}>
+        <CarouselArrow carouselRef={carouselRef} direction="left" />
+        <CarouselArrow carouselRef={carouselRef} direction="right" />
       </div>
     </article>
   );
--- src/components/carousel/carousel-item/index.tsx
+++ src/components/carousel/carousel-item/index.tsx
@@ -1,29 +1,47 @@
 import type { JSX } from "react";
+import { Link } from "react-router-dom";
 import { BsFeather } from "react-icons/bs";
+
+import type { ResponsePost } from "@/types/query";
 
 import styles from "./index.module.scss";
 
 interface Props {
-  postData: any;
-}
+  post: ResponsePost;
+};
 
-const CarouselItem = ({ postData }: Props): JSX.Element => {
+const CarouselItem = ({ post }: Props): JSX.Element => {
+  const {
+    _id,
+    thumbnailUrl,
+    writer,
+    title,
+    content
+  } = post;
+
   return (
-    <div className={styles["post-thumbnail-component"]}>
-      {postData.thumbnailImageSrc && (
+    <Link
+      to={`/post/${_id}`}
+      className={styles["carousel-item-component"]}
+    >
+      {thumbnailUrl && (
         <div className={styles["thumbnail-wrapper"]}>
-          <img src={postData.thumbnailImageSrc} className={styles["thumbnail"]} />
+          <img src={thumbnailUrl} className={styles["thumbnail"]} />
         </div>
       )}
       <div className={styles["title-wrapper"]}>
-        <h2 className={styles["title"]}>{postData.title}</h2>
+        <h2 className={styles["title"]}>
+          {title}
+        </h2>
       </div>
-      <div className={styles["content-wrapper"]}>{postData.content}</div>
+      <div className={styles["content-wrapper"]}>
+        {content}
+      </div>
       <div className={styles["creator-wrapper"]}>
         <BsFeather size={14} color="#959595" />
-        찡얼찡얼
+        {writer}
       </div>
-    </div>
+    </Link>
   );
 };
 
