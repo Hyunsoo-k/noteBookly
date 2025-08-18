779fe3439fc34f5d3f04ca2cbc85ea981ad45e9e
Hyunsoo_kim <red_panda119@naver.com>
2025-08-19 03:40:59 +0900

    Refactor  : 코드 단축

--- src/components/header/main-page-header/index.tsx
+++ src/components/header/main-page-header/index.tsx
@@ -3,13 +3,16 @@ import { useRef, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { PiMagnifyingGlassLight } from "react-icons/pi";
 
+import useSearchingModal from "@/hooks/searching-modal/use-searching-modal";
+
 import styles from "./index.module.scss";
 
 const MainPageHeader = (): JSX.Element | null => {
   const location = useLocation();
-  const isCreateOrEditPage
-    = location.pathname === "/create-post" || location.pathname.startsWith("/edit-post");
-
+  const isCreateOrEditPage =
+    location.pathname === "/create-post" ||
+    location.pathname.startsWith("/edit-post");
+  const { setIsOpen: setIsSearchModalOpen } = useSearchingModal();
   const headerRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
@@ -18,34 +21,36 @@ const MainPageHeader = (): JSX.Element | null => {
 
     const updateHeaderShadow = () => {
       const isScrollTop = window.scrollY === 0;
-
-      if (isScrollTop) {
-        $header.style.boxShadow = "none";
-      } else {
-        $header.style.boxShadow = "0 5px 5px #dddddd";
-      }
+      $header.style.boxShadow = isScrollTop
+        ? "none"
+        : "0 5px 5px #dddddd";
     };
-
     window.addEventListener("scroll", updateHeaderShadow);
 
-    return () => {
-      window.removeEventListener("scroll", updateHeaderShadow);
-    };
+    return () => { window.removeEventListener("scroll", updateHeaderShadow); };
   }, []);
 
-  if (isCreateOrEditPage) {
-    return null;
-  };
+  if (isCreateOrEditPage) return null;
 
   return (
     <header ref={headerRef} className={styles["main-page-header-component"]}>
       <div className={styles["content"]}>
-        <h1 className={styles["logo"]}>
-          <Link to="/">noteBookly</Link>
+        <h1 className={styles["banner"]}>
+          <Link to="/">
+            noteBookly
+          </Link>
         </h1>
-        <div className={styles["active-tool-box"]}>
-          <Link to="/create-post">글쓰기</Link>
-          <PiMagnifyingGlassLight size={25} />
+        <div className={styles["tool-box"]}>
+          <Link to="/create-post">
+            글쓰기
+          </Link>
+          <PiMagnifyingGlassLight
+            size={25}
+            onClick={(e) => {
+              e.stopPropagation();
+              setIsSearchModalOpen(true);
+            }}
+          />
         </div>
       </div>
     </header>
