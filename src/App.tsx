cfc47e33cddab1d1f6a468cf7fb161607508e3e5
Hyunsoo_kim <red_panda119@naver.com>
2025-08-19 03:51:44 +0900

    Refactor : 사용하지 않는 useState import 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, type JSX } from "react";
+import type { JSX } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
