556be44f80eb8a30ca13719c1abd962de8c71f0d
Hyunsoo_kim <red_panda119@naver.com>
2025-08-22 19:08:31 +0900

    Ui : 헤더 컴포넌트 ui 변경

--- src/components/header/create-or-edit-page-heder/index.tsx
+++ src/components/header/create-or-edit-page-heder/index.tsx
@@ -1,36 +1,11 @@
 import type { JSX } from "react";
-import { useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import styles from "./index.module.scss";
 
 const CreateOrEditPageHeader = (): JSX.Element => {
-  const headerRef = useRef<HTMLDivElement | null>(null);
-
-  useEffect(() => {
-    const $header = headerRef.current;
-    if (!$header) return;
-
-    const updateHeaderShadow = () => {
-      const isScrollTop = window.scrollY === 0;
-
-      if (isScrollTop) {
-        $header.style.boxShadow = "none";
-      } else {
-        $header.style.boxShadow = "0 5px 5px #dddddd";
-      }
-    };
-
-    window.addEventListener("scroll", updateHeaderShadow);
-
-    return () => { window.removeEventListener("scroll", updateHeaderShadow); }
-  }, []);
-
   return (
-    <header
-      ref={headerRef}
-      className={styles["create-or-edit-page-header-component"]}
-    >
+    <header className={styles["create-or-edit-page-header-component"]}>
       <h1 className={styles["banner"]}>
         <Link to="/">noteBookly</Link>
       </h1>
